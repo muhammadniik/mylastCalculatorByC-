d482d31fd6f318f475d930e80157f00e0add2f7a
mohammad <muhammad.mohammadi1377@gmail.com>
2022-02-13 21:10:52 +0330

    Add project files.

---
+++ mylastCalculator/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿
+namespace mylastCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btntaghsim = new System.Windows.Forms.Button();
+            this.btnZarb = new System.Windows.Forms.Button();
+            this.btnPoint = new System.Windows.Forms.Button();
+            this.btnEcule = new System.Windows.Forms.Button();
+            this.btnMines = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnPlas = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBackSpace = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btntaghsim);
+            this.groupBox1.Controls.Add(this.btnZarb);
+            this.groupBox1.Controls.Add(this.btnPoint);
+            this.groupBox1.Controls.Add(this.btnEcule);
+            this.groupBox1.Controls.Add(this.btnMines);
+            this.groupBox1.Controls.Add(this.btn8);
+            this.groupBox1.Controls.Add(this.btn5);
+            this.groupBox1.Controls.Add(this.btn2);
+            this.groupBox1.Controls.Add(this.btnPlas);
+            this.groupBox1.Controls.Add(this.btn7);
+            this.groupBox1.Controls.Add(this.btn4);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnBackSpace);
+            this.groupBox1.Controls.Add(this.btn9);
+            this.groupBox1.Controls.Add(this.btn6);
+            this.groupBox1.Controls.Add(this.btn3);
+            this.groupBox1.Controls.Add(this.btn0);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(12, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(448, 292);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btntaghsim
+            // 
+            this.btntaghsim.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntaghsim.Location = new System.Drawing.Point(339, 75);
+            this.btntaghsim.Name = "btntaghsim";
+            this.btntaghsim.Size = new System.Drawing.Size(105, 47);
+            this.btntaghsim.TabIndex = 0;
+            this.btntaghsim.Text = "/";
+            this.btntaghsim.UseVisualStyleBackColor = true;
+            this.btntaghsim.MouseClick += new System.Windows.Forms.MouseEventHandler(this.operands);
+            // 
+            // btnZarb
+            // 
+            this.btnZarb.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZarb.Location = new System.Drawing.Point(339, 128);
+            this.btnZarb.Name = "btnZarb";
+            this.btnZarb.Size = new System.Drawing.Size(105, 47);
+            this.btnZarb.TabIndex = 0;
+            this.btnZarb.Text = "*";
+            this.btnZarb.UseVisualStyleBackColor = true;
+            this.btnZarb.MouseClick += new System.Windows.Forms.MouseEventHandler(this.operands);
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPoint.Location = new System.Drawing.Point(339, 181);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(105, 47);
+            this.btnPoint.TabIndex = 0;
+            this.btnPoint.Text = ".";
+            this.btnPoint.UseVisualStyleBackColor = true;
+            this.btnPoint.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btnEcule
+            // 
+            this.btnEcule.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEcule.Location = new System.Drawing.Point(339, 234);
+            this.btnEcule.Name = "btnEcule";
+            this.btnEcule.Size = new System.Drawing.Size(105, 47);
+            this.btnEcule.TabIndex = 0;
+            this.btnEcule.Text = "=";
+            this.btnEcule.UseVisualStyleBackColor = true;
+            this.btnEcule.Click += new System.EventHandler(this.btnEcule_Click);
+            // 
+            // btnMines
+            // 
+            this.btnMines.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMines.Location = new System.Drawing.Point(228, 75);
+            this.btnMines.Name = "btnMines";
+            this.btnMines.Size = new System.Drawing.Size(105, 47);
+            this.btnMines.TabIndex = 0;
+            this.btnMines.Text = "-";
+            this.btnMines.UseVisualStyleBackColor = true;
+            this.btnMines.MouseClick += new System.Windows.Forms.MouseEventHandler(this.operands);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(228, 128);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(105, 47);
+            this.btn8.TabIndex = 0;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(228, 181);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(105, 47);
+            this.btn5.TabIndex = 0;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(228, 234);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(105, 47);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btnPlas
+            // 
+            this.btnPlas.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlas.Location = new System.Drawing.Point(117, 75);
+            this.btnPlas.Name = "btnPlas";
+            this.btnPlas.Size = new System.Drawing.Size(105, 47);
+            this.btnPlas.TabIndex = 0;
+            this.btnPlas.Text = "+";
+            this.btnPlas.UseVisualStyleBackColor = true;
+            this.btnPlas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.operands);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(117, 128);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(105, 47);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(117, 181);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(105, 47);
+            this.btn4.TabIndex = 0;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(117, 234);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(105, 47);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(228, 22);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(216, 47);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnBackSpace
+            // 
+            this.btnBackSpace.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackSpace.Location = new System.Drawing.Point(6, 22);
+            this.btnBackSpace.Name = "btnBackSpace";
+            this.btnBackSpace.Size = new System.Drawing.Size(216, 47);
+            this.btnBackSpace.TabIndex = 0;
+            this.btnBackSpace.Text = "<-------";
+            this.btnBackSpace.UseVisualStyleBackColor = true;
+            this.btnBackSpace.Click += new System.EventHandler(this.btnBackSpace_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(6, 75);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(105, 47);
+            this.btn9.TabIndex = 0;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(6, 128);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(105, 47);
+            this.btn6.TabIndex = 0;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(6, 181);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(105, 47);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(6, 234);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(105, 47);
+            this.btn0.TabIndex = 0;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.MouseClick += new System.Windows.Forms.MouseEventHandler(this.number);
+            // 
+            // txtShow
+            // 
+            this.txtShow.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtShow.Location = new System.Drawing.Point(9, 12);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.ReadOnly = true;
+            this.txtShow.Size = new System.Drawing.Size(445, 40);
+            this.txtShow.TabIndex = 1;
+            this.txtShow.TextChanged += new System.EventHandler(this.txtShow_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(18, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "on";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(466, 478);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Enter += new System.EventHandler(this.btnEcule_Click);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btntaghsim;
+        private System.Windows.Forms.Button btnZarb;
+        private System.Windows.Forms.Button btnPoint;
+        private System.Windows.Forms.Button btnEcule;
+        private System.Windows.Forms.Button btnMines;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnPlas;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnBackSpace;
+        private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ mylastCalculator/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mylastCalculator
+{
+    public partial class Form1 : Form
+    {
+        double x, y, z;
+        string operatorname = "";
+        bool iscleartxt = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEcule_Click(object sender, EventArgs e)
+        {
+            y = double.Parse(txtShow.Text);
+            switch (operatorname)
+            {
+                case "+":
+                    z = x + y;
+                    break;
+                case "-":
+                    z = x - y;
+                    break;
+                case "/":
+                    z = x / y;
+                    break;
+                case "*":
+                    z = x * y;
+                    break;
+            }
+            txtShow.Text = z.ToString();
+            operatorname = "";
+            z = 0;
+            y = 0;
+            x = 0;
+
+        }
+
+
+        private void number(object sender, MouseEventArgs e)
+        {
+            if (iscleartxt)
+            {
+                txtShow.Text = "";
+                iscleartxt = false;
+            }
+            txtShow.Text += ((Button)sender).Text;
+        }
+        private void btnBackSpace_Click(object sender, EventArgs e)
+        {
+
+            txtShow.Text = txtShow.Text.Remove(txtShow.Text.Length - 1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtShow_TextChanged(null, null);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtShow.Text = "";
+            y = 0;
+            x = 0;
+            y = 0;
+
+
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Button test = new Button();
+            test.Text = e.KeyChar.ToString();
+
+            if (e.KeyChar >= '0' & e.KeyChar <= '9')
+            {
+                number(test, null);
+            }
+            else if (e.KeyChar == '.' && !txtShow.Text.Contains('.'))
+            {
+                number(test, null);
+            }
+            else if (e.KeyChar == '+' || e.KeyChar == '-' || e.KeyChar == '*' || e.KeyChar == '/')
+            {
+                operands(test, null);
+            }
+            else if (e.KeyChar == '\b' && txtShow.Text.Length > 0)
+            {
+                btnBackSpace_Click(null, null);
+            }
+            else if (e.KeyChar == '=')
+            {
+                btnEcule_Click(null, null);
+            }
+
+
+            foreach (Button name in groupBox1.Controls)
+            {
+                if (e.KeyChar.ToString() == name.Text)
+                {
+                    name.Focus();
+                    name.ForeColor = Color.Red;
+                }
+                else
+                {
+                    name.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = !groupBox1.Enabled;
+            this.KeyPreview = !this.KeyPreview;
+            
+            if(button1.Text == "on")
+            {
+                button1.Text = "off";
+                
+            }
+            else
+            {
+                button1.Text = "on";
+                btnClear_Click(null, null);
+            }
+        }
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string a = e.KeyChar.ToString();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnEcule_Click(null, null);
+                foreach (Button name in groupBox1.Controls)
+                {
+                    if (name.Text == "=")
+                    {
+                        name.Focus();
+                        name.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        name.ForeColor = Color.Black;
+                    }
+                }
+            }
+
+        }
+
+        private void txtShow_TextChanged(object sender, EventArgs e)
+        {
+            btnPoint.Enabled = !(txtShow.Text.Contains("."));
+            btnBackSpace.Enabled = Convert.ToBoolean(txtShow.Text.Length);
+            btnEcule.Enabled = Convert.ToBoolean(txtShow.Text.Length);
+
+        }
+
+        private void operands(object sender, MouseEventArgs e)
+        {
+
+            if (operatorname != "")
+            {
+                btnEcule_Click(null, null);
+
+            }
+
+            x = double.Parse(txtShow.Text);
+            iscleartxt = true;
+            operatorname = ((Control)sender).Text;
+
+
+
+        }
+    }
+}
